04b0f6083f1b1f71e225caf75c4c4658f434a563
kneeraazon01 <karkinirajan1999@gmail.com>
2020-01-17 23:53:41 +0545

    users register and login logout done

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        messages.error(request, "testing error message")
+        return redirect("register")
+    else:
+        return render(request, "users/register.html")
+
+
+def login(request):
+    return render(request, "users/login.html")
+
+
+def logout(request):
+    return redirect("home")
+
+
+def profile(request):
+    return render(request, "users/profile.html")
