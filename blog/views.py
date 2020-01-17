36f8d775734ed568199c3bfa4ec3ac72615ef2af
kneeraazon01 <karkinirajan1999@gmail.com>
2020-01-17 16:58:17 +0545

    carousel not sliding problem fixed

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from team.models import Team
 from posts.models import Post
-
+  
 # Create your views here.
 def index(request):
-    posts = Post.objects.order_by("-post_date")
+    posts = Post.objects.order_by("-post_date")[:3]
     context = {"posts": posts}
     return render(request, "blog/index.html", context)
 
