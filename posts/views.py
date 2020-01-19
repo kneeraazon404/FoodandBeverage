8b8c566c9a659dfc93af806ce452ab31c145b26e
kneeraazon01 <karkinirajan1999@gmail.com>
2020-01-20 01:13:51 +0545

    requirements.txt edited

--- posts/views.py
+++ posts/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator
 import request
+import re
+from django import template
+from django.conf import settings
+
+register = template.Library()
 
 # Create your views here.
 
@@ -15,6 +20,26 @@ def posts(request):
     paged_posts = paginator.get_page(page_number)
     context = {"posts": paged_posts}
     return render(request, "posts/posts.html", context)
+
+
+@register.filter(name="youtube_embed_url")
+# converts youtube URL into embed HTML
+# value is url
+def youtube_embed_url(value):
+    match = re.search(
+        r"^(http|https)\:\/\/www\.youtube\.com\/watch\?v\=(\w*)(\&(.*))?$", value
+    )
+    if match:
+        embed_url = "http://www.youtube.com/embed/%s" % (match.group(2))
+        res = (
+            '<iframe width="560" height="315" src="%s" frameborder="0" allowfullscreen></iframe>'
+            % (embed_url)
+        )
+        return res
+    return ""
+
+
+youtube_embed_url.is_safe = True
 
 
 def post(request, post_id):
