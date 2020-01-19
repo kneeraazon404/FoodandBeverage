244956c8a6064e6379311961a0403c39eed6deb6
kneeraazon01 <karkinirajan1999@gmail.com>
2020-01-20 01:35:09 +0545

    untrackked fixed

---
+++ FoodandBeverage/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("blog.urls")),
+    path("contact/", include("contact.urls")),
+    path("posts/", include("posts.urls")),
+    path("users/", include("users.urls")),
+    path("team/", include("team.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
