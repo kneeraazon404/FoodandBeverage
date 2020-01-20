67f4ddf565e213fe4597a079a25305f3567c3214
kneeraazon01 <karkinirajan1999@gmail.com>
2020-01-20 17:54:57 +0545

    migrations deleted in contact app

--- FoodandBeverage/settings.py
+++ FoodandBeverage/settings.py
@@ -20,6 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "3c5#!dl+@)a9o0q+0x1=(i3q%4e_ab%w#*unc@ut-y-%k+lm3j"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 
 # Application definition
 
@@ -71,6 +79,16 @@ WSGI_APPLICATION = "FoodandBeverage.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "foodandbeverage_db",
+        "USER": "postgres",
+        "PASSWORD": "karki582465",
+        "HOST": "localhost",
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -117,6 +135,12 @@ from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {messages.ERROR: "danger"}
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "karkinirajan1999@gmail.com"
+EMAIL_HOST_PASSWORD = "karki582465@#$%"
 
 try:
     from .local_settings import *
