b18169b3366cc725dcdd102f0e34f1b0ae15523e
kneeraazon01 <karkinirajan1999@gmail.com>
2020-03-28 17:04:10 +0545

    settings updated

--- FoodandBeverage/settings.py
+++ FoodandBeverage/settings.py
@@ -1,25 +1,14 @@
-
-
 import os
 
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "3c5#!dl+@)a9o0q+0x1=(i3q%4e_ab%w#*unc@ut-y-%k+lm3j"
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
-# Application definition
 
 INSTALLED_APPS = [
     "users.apps.UsersConfig",
@@ -66,9 +55,6 @@ TEMPLATES = [
 WSGI_APPLICATION = "FoodandBeverage.wsgi.application"
 
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -79,10 +65,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
@@ -91,10 +73,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
@@ -110,7 +88,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "FoodandBeverage/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "food/static")]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
@@ -119,15 +97,3 @@ MEDIA_URL = "/media/"
 from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {messages.ERROR: "danger"}
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "karkinirajan1999@gmail.com"
-EMAIL_HOST_PASSWORD = "karki582465@#$%"
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
