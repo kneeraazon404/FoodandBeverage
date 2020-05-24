d98c18bc2c6f0592644e8c9baf8bf7af2b87c8c8
kneeraazon01 <karkinirajan1999@gmail.com>
2020-05-25 03:47:28 +0545

    -finishde

--- FoodandBeverage/settings.py
+++ FoodandBeverage/settings.py
@@ -3,12 +3,12 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY = "3c5#!dl+@)a9o0q+0x1=(i3q%4e_ab%w#*unc@ut-y-%k+lm3j"
+SECRET_KEY = "3c5#!dl+@)a9o0q(i3q%4e_ab%w#*unc@ut-y-%k+lm3j"
 
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["foodandbeverage.azurewebsites.net"]
 
 
 INSTALLED_APPS = [
